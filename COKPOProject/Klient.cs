6af5390617be4f1d7a3134e407f1b902da6bf55b
Julian <julian.zielinski.01@gmail.com>
2021-05-21 15:01:55 +0200

    Stworzenie klasy Klient

---
+++ COKPOProject/Klient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COKPOProject
+{
+    abstract class Klient
+    {
+        public readonly int IdKlienta;
+        public readonly Bank BankKlienta; //UWAGA BłĄD TYMCZASOWY (BANK)
+        protected string NazwaKlienta;
+        protected List<Karta> Karty; //UWAGA BŁĄD TYMCZASOWY (KARTA)
+
+        public List<Karta> getKarty() => Karty;
+
+        public string getNazwa() => NazwaKlienta;
+
+        public void setNazwa(string Nazwa)
+        {
+            this.NazwaKlienta = Nazwa;
+        }
+        public void usunKarte(int index)
+        {
+            //Dodac wyjatek!!!!
+            Karty.Remove(Karty[index - 1]);
+        }
+        public void dodajKarte()
+        {
+            //Uzupełnić po napisaniu klasy KARTA!!!  
+        }
+        public virtual void RejestrujTransakcje(decimal Kwota, string NrKarty) { }
+    }
+}
