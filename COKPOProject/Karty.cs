ef7ba63cda882ecb3ce4850cf46f095d1047e900
dandmano <demoon.pps@gmail.com>
2021-05-21 15:30:20 +0200

    Dokończone karty

--- COKPOProject/Karty.cs
+++ COKPOProject/Karty.cs
@@ -16,7 +16,18 @@ namespace COKPOProject
         public decimal GetSaldo() => Saldo;
         public void SetSaldo(decimal Kwota) { Saldo = Kwota; }
         public abstract bool CzyWystarczajaceSaldo(decimal Kwota);
-        public Karta(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty)
+        protected Karta(Klient Wlasciciel, Bank BankWydajacy)
+        {
+            this.Wlasciciel = Wlasciciel;
+            this.BankWydajacy = BankWydajacy;
+        }
+        protected Karta(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo)
+        {
+            this.Wlasciciel = Wlasciciel;
+            this.BankWydajacy = BankWydajacy;
+            this.Saldo = Saldo;
+        }
+        protected Karta(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty)
         {
             this.Wlasciciel = Wlasciciel;
             this.BankWydajacy = BankWydajacy;
@@ -27,67 +38,36 @@ namespace COKPOProject
 
     class KartaDebetowa : Karta
     {
-        public KartaDebetowa(Klient Wlasciciel, Bank BankWydajacy)
-        {
-            this.Wlasciciel = Wlasciciel;
-            this.BankWydajacy = BankWydajacy;
-        }
-        public KartaDebetowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo)
-        {
-            this.Wlasciciel = Wlasciciel;
-            this.BankWydajacy = BankWydajacy;
-            this.Saldo = Saldo;
-        }
-        public KartaDebetowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty) : base(Wlasciciel, BankWydajacy, Saldo, NrKarty)
-        {
+        public KartaDebetowa(Klient Wlasciciel, Bank BankWydajacy) : base(Wlasciciel, BankWydajacy) { }
+        public KartaDebetowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo) : base(Wlasciciel, BankWydajacy, Saldo) { }
 
-        }
-        public override bool CzyWystarczajaceSaldo(decimal Kwota)
-        {
+        public KartaDebetowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty) : base(Wlasciciel, BankWydajacy, Saldo, NrKarty) { }
 
-        }
+        public override bool CzyWystarczajaceSaldo(decimal Kwota) => Kwota < Saldo;
+
     }
 
     class KartaKredytowa : Karta
     {
-        private decimal LimitKredytu;
-        public KartaKredytowa(Klient Wlasciciel, Bank BankWydajacy)
-        {
+        private decimal LimitKredytu = 5000;
+        public KartaKredytowa(Klient Wlasciciel, Bank BankWydajacy) : base(Wlasciciel, BankWydajacy) { }
 
-        }
-        public KartaKredytowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo)
-        {
+        public KartaKredytowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo) : base(Wlasciciel, BankWydajacy, Saldo) { }
 
-        }
-        public KartaKredytowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty)
-        {
-
-        }
+        public KartaKredytowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty) : base(Wlasciciel, BankWydajacy, Saldo, NrKarty) { }
         public void SetLimitKredytu(decimal Kwota) { LimitKredytu = Kwota; }
         public decimal GetLimitKredytu() => LimitKredytu;
-        public override bool CzyWystarczajaceSaldo(decimal Kwota)
-        {
-
-        }
+        public override bool CzyWystarczajaceSaldo(decimal Kwota) => Kwota < Saldo + LimitKredytu;
     }
 
     class KartaBankomatowa : Karta
     {
-        public KartaBankomatowa(Klient Wlasciciel, Bank BankWydajacy)
-        {
+        public KartaBankomatowa(Klient Wlasciciel, Bank BankWydajacy) : base(Wlasciciel, BankWydajacy) { }
 
-        }
-        public KartaBankomatowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo)
-        {
+        public KartaBankomatowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo) : base(Wlasciciel, BankWydajacy, Saldo) { }
 
-        }
-        public KartaBankomatowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty)
-        {
+        public KartaBankomatowa(Klient Wlasciciel, Bank BankWydajacy, decimal Saldo, string NrKarty) : base(Wlasciciel, BankWydajacy, Saldo, NrKarty) { }
 
-        }
-        public override bool CzyWystarczajaceSaldo(decimal Kwota)
-        {
-
-        }
+        public override bool CzyWystarczajaceSaldo(decimal Kwota) => false;
     }
 }
