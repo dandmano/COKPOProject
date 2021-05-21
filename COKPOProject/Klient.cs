58ab191b2badaede4a65f64a5e1f1fb33dfd8544
dandmano <demoon.pps@gmail.com>
2021-05-21 17:34:23 +0200

    Dodanie klas KlientCentrum oraz KlientZwykly

--- COKPOProject/Klient.cs
+++ COKPOProject/Klient.cs
@@ -9,9 +9,9 @@ namespace COKPOProject
     abstract class Klient
     {
         public readonly int IdKlienta;
-        public readonly Bank BankKlienta; //UWAGA BłĄD TYMCZASOWY (BANK)
+        public readonly Bank BankKlienta;
         protected string NazwaKlienta;
-        protected List<Karta> Karty; //UWAGA BŁĄD TYMCZASOWY (KARTA)
+        protected List<Karta> Karty;
 
         public List<Karta> getKarty() => Karty;
 
@@ -26,10 +26,43 @@ namespace COKPOProject
             //Dodac wyjatek!!!!
             Karty.Remove(Karty[index - 1]);
         }
-        public void dodajKarte()
+        public void dodajKarte(int wybor)
         {
-            //Uzupełnić po napisaniu klasy KARTA!!!  
+            switch (wybor)
+            {
+                case 1:
+                    Karty.Add(new KartaDebetowa(this, BankKlienta));
+                    break;
+                case 2:
+                    Karty.Add(new KartaKredytowa(this, BankKlienta));
+                    break;
+                case 3:
+                    Karty.Add(new KartaBankomatowa(this, BankKlienta));
+                    break;
+            }
         }
-        public virtual void RejestrujTransakcje(decimal Kwota, string NrKarty) { }
+        public virtual void RejestrujTransakcje(decimal Kwota, string NrKarty)
+        {
+            //Dodac pop-up w intefejscie graficznym z błedęm kart bankomatowej
+        }
+        public Klient(string NazwaKlienta, Bank BankKlienta)
+        {
+            this.NazwaKlienta = NazwaKlienta;
+            this.BankKlienta = BankKlienta;
+        }
+    }
+
+    class KlientCentrum : Klient
+    {
+        public KlientCentrum(string NazwaKlienta, Bank BankKlienta) : base(NazwaKlienta, BankKlienta) { }
+        public override void RejestrujTransakcje(decimal Kwota, string NrKarty)
+        {
+            //Dodac rejestracje transakcji
+        }
+    }
+
+    class KlientZwykly : Klient
+    {
+        public KlientZwykly(string NazwaKlienta, Bank BankKlienta) : base(NazwaKlienta, BankKlienta) { }
     }
 }
