dd8ae477729a1396fce885fd12a6d911c04f63fc
dandmano <demoon.pps@gmail.com>
2021-05-30 20:39:33 +0200

    Naprawiona kropka we wpisywaniu nr karty w dodawaniu klienta i karty

--- COKPOProject/FDodajKlientaPopUp.cs
+++ COKPOProject/FDodajKlientaPopUp.cs
@@ -121,7 +121,7 @@ namespace COKPOProject
         private void CardNumberTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
             // Verify that the pressed key isn't CTRL or any non-numeric digit
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
             {
                 e.Handled = true;
             }
