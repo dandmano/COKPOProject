4e98c471b634aa5e12b9f37f6afeb4a3aa06545f
dandmano <demoon.pps@gmail.com>
2021-06-16 22:22:22 +0200

    napriwlem testowe metody

--- COKPOProject/Program.cs
+++ COKPOProject/Program.cs
@@ -18,24 +18,5 @@ namespace COKPOProject
             Application.Run(new FStronaGlowna(centrumTransakcji));
         }
 
-        /*private static void Stworzbankitd(CentrumTransakcji centrumTransakcji)
-        {
-            centrumTransakcji.DodajBank("Millenium");
-            centrumTransakcji.DodajBank("Alior");
-            centrumTransakcji.Banki[0].DodajKlienta("Jan Kowalski", 2);
-            centrumTransakcji.Banki[0].Klienci[0].DodajKarte(0, 1500, "");
-            centrumTransakcji.Banki[0].Klienci[0].DodajKarte(0, 25, "");
-            centrumTransakcji.Banki[0].DodajKlienta("Mlekpol", 1);
-            centrumTransakcji.Banki[0].Klienci[1].DodajKarte(1, 13000, "");
-            centrumTransakcji.Banki[0].Klienci[1].DodajKarte(0, 50, "");
-            centrumTransakcji.Banki[1].DodajKlienta("Leon Bober", 2);
-            centrumTransakcji.Banki[1].Klienci[0].DodajKarte(0, 69, "");
-            centrumTransakcji.Banki[1].Klienci[0].DodajKarte(1, 6969, "");
-            centrumTransakcji.Banki[1].DodajKlienta("Adidasz", 1);
-            centrumTransakcji.Banki[1].Klienci[1].DodajKarte(1, 1513214, "");
-            centrumTransakcji.Banki[1].Klienci[1].DodajKarte(0, 3232, "");
-            centrumTransakcji.DodajTransakcje((Firma)centrumTransakcji.Banki[0].Klienci[1], 690, centrumTransakcji.Banki[1].Klienci[0].Karty[1].NrKarty);
-            centrumTransakcji.DodajTransakcje((Firma)centrumTransakcji.Banki[1].Klienci[1], 6969, centrumTransakcji.Banki[0].Klienci[0].Karty[0].NrKarty);
-        }*/
     }
 }
