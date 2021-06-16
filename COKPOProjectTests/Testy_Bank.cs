2f82704083f995561a5b5c96a9a0d58120f8b472
dandmano <demoon.pps@gmail.com>
2021-06-16 22:51:08 +0200

    Dodano jeden test do baknu

--- COKPOProjectTests/Testy_Bank.cs
+++ COKPOProjectTests/Testy_Bank.cs
@@ -8,8 +8,11 @@ namespace COKPOProjectTests
     {
 
         [TestMethod]
-        public void idk()
+        public void DodajBank_ExpectedExisitngKlient()
         {
+            var ct = MetodyDoTestow.StworzTestoweDane();
+            ct.Banki[0].DodajKlienta("Rysiek z klanu", 0, 0, 1200, "");
+            Assert.IsTrue(ct.Banki[0].Klienci[2].NazwaKlienta == "Rysiek z klanu");
         }
     }
 }
